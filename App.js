7444a5daf17666556981d8e2dad0e7cf10f1395c
Stadionova <jtribunskaja@hamkorlab.club>
2023-10-28 21:50:45 +0200

    created new View components for header, content and footer

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 // import {StatusBar} from 'expo-status-bar';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>ЧАО</Text>
-            {/*<StatusBar style="auto" />*/}
-            <Button title={'CLICK ON ME'}/>
+        <View style={styles.appContainer}>
+            <View style={styles.title}>
+                <Text style={styles.text}>KING FOR A DAY</Text>
+            </View>
+            <View style={styles.content}>
+                <TextInput placeholder={'type your real name or nickname'}/>
+            </View>
+            <View style={styles.footer}>
+                <Button title={'CLICK TO GET GOAL FOR A DAY'}/>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    appContainer: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    title: {
+        marginLeft: 20,
+        padding: 16
+    },
+    content: {
+        marginLeft: 20,
+        marginBottom: 16
+    },
+    footer: {
+        marginLeft: 20,
+        borderColor: 'blue',
+        borderWidth: 2,
+        padding: 8,
+        backgroundColor: 'pink',
+        textDecorationColor: 'white'
+    }
 });
