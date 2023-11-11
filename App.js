2ca41d3e6e0f7775d43545f2475ecdee1d5dc670
Stadionova <jtribunskaja@hamkorlab.club>
2023-11-11 20:58:38 +0100

    changed styles of test app

--- App.js
+++ App.js
@@ -4,14 +4,24 @@ import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 export default function App() {
     return (
         <View style={styles.appContainer}>
-            <View style={styles.title}>
-                <Text style={styles.text}>KING FOR A DAY</Text>
+            <View style={styles.titleWrapper}>
+                <View style={{marginLeft: 20, marginRight: 10}}>
+                    <Text style={styles.text}>KING FOR A DAY</Text>
+                </View>
+                <View style={styles.title}>
+                    <Text style={styles.text}>BE READY TO REACH YOUR GOAL</Text>
+                </View>
             </View>
             <View style={styles.content}>
-                <TextInput placeholder={'type your real name or nickname'}/>
+                <TextInput placeholder={'write your real name or nickname'}/>
             </View>
-            <View style={styles.footer}>
-                <Button title={'CLICK TO GET GOAL FOR A DAY'}/>
+            <View>
+                <View style={styles.footer}>
+                    <Button title={'CLICK TO GET GOAL FOR A DAY'}/>
+                </View>
+                <View style={styles.footer}>
+                    <Button title={'OK'}/>
+                </View>
             </View>
         </View>
     );
@@ -20,13 +30,16 @@ export default function App() {
 const styles = StyleSheet.create({
     appContainer: {
         flex: 1,
+        // flexDirection: 'row',
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    title: {
-        marginLeft: 20,
-        padding: 16
+    titleWrapper: {
+        // flexDirection: 'row',
+        flexDirection: 'column',
+        // justifyContent: 'space-between',
+        paddingBottom: 8
     },
     content: {
         marginLeft: 20,
@@ -38,6 +51,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         padding: 8,
         backgroundColor: 'pink',
-        textDecorationColor: 'white'
+        textDecorationColor: 'white',
+        marginBottom: 20
     }
 });
